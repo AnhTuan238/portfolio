f38500b39e9cc660c0f4587475f2cd8fdd9e207a
AnhTuan238 <hoanhtuan23082003@gmail.com>
2026-03-06 09:20:32 +0700

    chore: change alias import

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -2,7 +2,7 @@ import { QueryClientProvider } from "@tanstack/react-query";
 import { createRoot } from "react-dom/client";
 import { StrictMode } from "react";
 
-import { queryClient } from "@/shared/config/index.ts";
+import { queryClient } from "@/shared";
 import App from "./App.tsx";
 
 createRoot(document.getElementById("root")!).render(
--- client/src/shared/index.ts
+++ client/src/shared/index.ts
@@ -1,3 +1,4 @@
 export * from "./components";
 export * from "../features/auth/queries";
 export * from "./utils";
+export * from "./config";
