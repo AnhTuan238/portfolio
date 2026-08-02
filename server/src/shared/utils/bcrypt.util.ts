f5ab0a02d7a21dae96b0a467609c2707f827404a
AnhTuan238 <hoanhtuan23082003@gmail.com>
2026-08-02 07:37:42 +0700

    refactor: refactor code

--- server/src/shared/utils/bcrypt.util.ts
+++ server/src/shared/utils/bcrypt.util.ts
@@ -2,7 +2,7 @@ import bcrypt from "bcryptjs";
 
 export const compareValue = async (
   value: string,
-  hashedValue: string
+  hashedValue: string,
 ): Promise<boolean> => await bcrypt.compare(value, hashedValue);
 
 export const hashValue = async (value: string): Promise<string> =>
